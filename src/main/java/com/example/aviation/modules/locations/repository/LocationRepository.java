13a93e12ae39c142da687f98c198e904cae0f0a9
muhal-git <iris.muhammet.metu@gmail.com>
2025-03-26 18:48:46 +0300

    comments regarding @Repository annotation in LocationRepository

--- src/main/java/com/example/aviation/modules/locations/repository/LocationRepository.java
+++ src/main/java/com/example/aviation/modules/locations/repository/LocationRepository.java
@@ -3,9 +3,12 @@ package com.example.aviation.modules.locations.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+// You could add @Repository, but it's not required
+// import org.springframework.stereotype.Repository;
 
 import com.example.aviation.modules.locations.entity.Location;
 
+// @Repository annotation is optional here since the interface extends JpaRepository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
     Optional<Location> findByLocationCode(String locationCode);
