b2bd2e308e6ca1f4ed8b5098477db160956c58d7
muhal-git <iris.muhammet.metu@gmail.com>
2025-03-27 14:53:10 +0300

    Remove custom equals and hashCode methods from BaseEntity class

--- src/main/java/com/example/aviation/common/entity/BaseEntity.java
+++ src/main/java/com/example/aviation/common/entity/BaseEntity.java
@@ -51,18 +51,4 @@ public class BaseEntity implements Serializable {
     @LastModifiedBy
     private String updatedBy;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        
-        BaseEntity that = (BaseEntity) o;
-        
-        return id != null && id.equals(that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
 }
